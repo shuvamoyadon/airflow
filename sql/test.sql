b63f1be8a5b83f41a746039d4987d1ba77d9e5e8
shuvamoyadon <42385240+shuvamoyadon@users.noreply.github.com>
2025-11-26 16:25:17 +0530

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -1,20 +1,19 @@
+-- sql/src_customer.sql
 
-CREATE OR REPLACE TABLE `edp-dev-carema.edp_ent_cma_plss_onboarding_src.src_customer`
+CREATE OR REPLACE TABLE `{{ project_name }}.{{ dataset }}.src_customer`
 (
-  customer_id        STRING      NOT NULL,
-  sources     STRING,
-  first_name         STRING,
-  last_name          STRING,
-  email              STRING,
-  phone_number       STRING,
-  country_code       STRING,
-  created_at         TIMESTAMP   NOT NULL,
-  updated_at         TIMESTAMP,
-  is_active          BOOL        DEFAULT TRUE,
-
-  -- optional metadata columns
-  batch_id           STRING,
-  ingestion_timestamp TIMESTAMP  DEFAULT CURRENT_TIMESTAMP()
+  customer_id         STRING      NOT NULL,
+  sources             STRING,
+  first_name          STRING,
+  last_name           STRING,
+  email               STRING,
+  phone_number        STRING,
+  country_code        STRING,
+  created_at          TIMESTAMP   NOT NULL,
+  updated_at          TIMESTAMP,
+  is_active           BOOL        DEFAULT TRUE,
+  batch_id            STRING,
+  ingestion_timestamp TIMESTAMP   DEFAULT CURRENT_TIMESTAMP()
 )
 PARTITION BY DATE(created_at)
 CLUSTER BY customer_id;
